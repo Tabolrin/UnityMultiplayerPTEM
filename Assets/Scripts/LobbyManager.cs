337f41dccf232308e593fe1558722aefafa3d242
Tabolrin <93069446+Tabolrin@users.noreply.github.com>
2025-05-23 18:47:54 +0300

    Dis call progress
    - added lobbies
    - added player count and custom session name
    - i dont remember - see changes
    
    Co-Authored-By: Maayan <170449297+Maayana24@users.noreply.github.com>
    Co-Authored-By: TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
    Co-Authored-By: thaib2 <170455912+thaib2@users.noreply.github.com>

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,18 +1,35 @@
 using Fusion;
+using Fusion.Sockets;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Multiplayer;
 
 
 public class LobbyManager : MonoBehaviour
 {
-    [SerializeField] NetworkRunner networkRunner;
+    const string catsLobby = "Cats";
+    const string puppiesLobby = "Puppies";
+    const string capysLobby = "Capybara's";
 
-    public async void StartSession()
+    private string currentLobby;
+    
+    [SerializeField] NetworkRunner networkRunner;
+    
+    [SerializeField] private Button startSessionButton;
+    [SerializeField] private Button endSessionButton;
+    [SerializeField] private TextMeshProUGUI numberOfPlayersText;
+
+    
+    public async void StartSession(int playerLimit)
     {
         StartGameResult resTask = await networkRunner.StartGame(new StartGameArgs()
         {
             GameMode = GameMode.Shared,
             SessionName = "Lobby",
-            OnGameStarted = OnGameStarted
+            PlayerCount = 4,
+            OnGameStarted = OnGameStarted,
+            CustomLobbyName = currentLobby,
         });
 
         if (resTask.Ok)
@@ -20,6 +37,22 @@ public class LobbyManager : MonoBehaviour
         else
         {
             Debug.LogError($"Game start failed: {resTask.ShutdownReason}");
+        }
+    }
+    
+    
+    public async void JoinLobby(string lobbyName)
+    {
+        StartGameResult result = await networkRunner.JoinSessionLobby(SessionLobby.Custom, lobbyName);
+     
+        if (result.Ok)
+        {
+            Debug.Log("Joined Lobby!");
+            currentLobby = lobbyName;
+        }
+        else
+        {
+            Debug.LogError($"Game join failed: {result.ShutdownReason}");
         }
     }
 
